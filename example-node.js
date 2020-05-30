dde6d6cb44287f089a7b70621fd5b2039b5cf8d6
Elias Aguilera <200393648@student.georgianc.on.ca>
2020-05-30 11:26:34 -0400

    First app

---
+++ example-node.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const https = require('https');
+
+app.get('/',(request, response)=> {
+   https.get('https://insult.mattbas.org/api/insult', resp =>{
+       resp.on('data', chunk => {
+           response.send(`
+           <http>
+           <head><title>Oh Snap</title>
+           <h1>OH SNAP!</h1>
+           <p style="font-size:24px; color:red">
+            ${chunk.toString()}!
+           </p>
+           <body></body>
+           </http>
+           `)
+       })
+   })
+});
+
+app.listen(3000);
