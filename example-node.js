39502134387a961594714b4e101bdc32696b55e7
Elias Aguilera <200393648@student.georgianc.on.ca>
2020-05-31 02:21:54 -0400

    changes 1

--- example-node.js
+++ example-node.js
@@ -14,7 +14,7 @@ app.get('/',(request, response)=> {
            <p style="font-size:24px; color:red">
             ${chunk.toString()}!
            </p>
-           <img src="./images/bean${randomPic}.jpg" alt="Girl in a jacket" width="500" height="600">
+           <img src="./images/bean1.jpg" alt="Girl in a jacket" width="500" height="600">
            </body>
            </http>
            `)
